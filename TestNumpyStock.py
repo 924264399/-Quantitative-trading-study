b89f6417e93098dc483f9781c16a1c46c82bfeea
刘耀宇 <924264399@qq.com>
2025-12-16 08:13:27 +0800

    1216 numpy study

---
+++ TestNumpyStock.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+
+class TsetNumpyStock(TestCase):
+    """读取指定列
+        numpy.loadtxt需要传入4个关键字参数:
+        1.fname是文件名，数据类型为字符串str:
+        2.delimiter是分隔符，数据类型为字符str;
+        3.usecols是读取的列数，数据类型为元组tuple，其中元素个数有多少个，则选出多少列:
+        4.unpack是是否解包，数据类型为布尔bo0l.
+    #"""
+
+    def testReadFile(self):
+        file_name = "./demo.csv"
+
+
+
+    #计算最大最小值
+    def testMaxAndMin(self):
+        
